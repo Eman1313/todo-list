08ed70381e43b9c1e7f1cb982e216835e34699db
Eman1313 <63458380+Eman1313@users.noreply.github.com>
2020-04-19 00:18:42 +0600

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,73 @@
+package test;
+import java.util.Date;
+
+class Task extends Date {
+	private int id; 
+	private String taskName; 
+	private boolean isDone; 
+	private Date taskCreated, taskCompleted, deadline;
+	private static int autoIncrementingId = 1;
+        public int getId()
+        {
+            return id;
+        }
+        public void setTaskName(String tsk)
+        {
+            taskName=tsk;
+        }
+        public String getTaskName()
+        {
+            return taskName;
+        }
+         public void setIsDone(boolean isd)
+        {
+            isDone=isd;
+        }
+        public boolean getIsDone()
+        {
+            return isDone;
+        }
+        public void setTaskCreated(Date tskcrd)
+        {
+            taskCreated=tskcrd;
+        }
+        public Date getTaskCreated()
+        {
+            return taskCreated;
+        }
+         public void setTaskCompleted(Date tskcmd)
+        {
+            taskCompleted=tskcmd;
+        }
+        public Date getTaskCompleted()
+        {
+            return taskCompleted;
+        }
+         public void setDeadLine(Date ddl)
+        {
+            deadline=ddl;
+        }
+        public Date getDeadLine()
+        {
+            return deadline;
+        }
+        
+
+	public Task(String taskName, Date deadline) {
+		this.taskName = taskName;
+		id = autoIncrementingId++;
+		this.deadline = deadline;
+		isDone = false;
+		taskCreated = new Date();
+	}
+}
+
+
+public class Test {
+
+   
+    public static void main(String[] args) {
+       
+    }
+    
+}
